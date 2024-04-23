b69ecabc301f05c897296096d61250e9a589db8c
Jluffy <julyhtwe6250@gmail.com>
2024-04-23 13:38:37 +0630

    aaa

---
+++ home.php
@@ -0,0 +1,29 @@
+
+<html>
+    <head>
+        <link rel="stylesheet" href="home.css">
+    </head>
+    <div>
+   <h1 style="text-align: center;">Welcome Admin!</h1>
+   <h3 style="text-align: center;color:red;">Admin informations</h3>
+    <table>
+        <tr>
+            <td>Name</td>
+            <td>Estes</td>
+        </tr>
+        
+        <tr>
+            <td>Address</td>
+            <td>Nay Pyi Taw, Lewe</td>
+        </tr>
+        <tr>
+            <td>Ph-no</td>
+            <td>09-968554726</td>
+        </tr>
+        <tr>
+            <td>Credit card no</td>
+            <td>012-235-854</td>
+        </tr>
+    </table>
+    </div>
+</html>
